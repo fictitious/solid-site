fc683c0659a40a80628357eacdd1f74d6d94526e
Artem Khodyush <greenkaa@gmail.com>
2022-02-21 12:34:19 -0800

    rename useData -> useRouteData (changed in solid-app-router 0.3.0)

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,6 +1,6 @@
 import { Component, For, Show, createSignal, createMemo } from 'solid-js';
 import { createStore } from 'solid-js/store';
-import { useData } from 'solid-app-router';
+import { useRouteData } from 'solid-app-router';
 import Footer from '../components/Footer';
 import { ResourcesDataProps } from './Resources.data';
 import { Icon } from 'solid-heroicons';
@@ -142,7 +142,7 @@ const Resource: Component<Resource> = (props) => {
 
 const Resources: Component = () => {
   const [t] = useI18n();
-  const data = useData<ResourcesDataProps>();
+  const data = useRouteData<ResourcesDataProps>();
   const fs = new Fuse(data.list, {
     keys: ['author', 'title', 'categories', 'keywords', 'link', 'description'],
     threshold: 0.3,
